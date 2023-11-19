5c1ed02c478e4bf7b6dac682f4bb91fa9f1e93e8
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-11-19 14:57:13 +0700

    Custom render function

--- src/components/mui/MuiMode.test.tsx
+++ src/components/mui/MuiMode.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from "@testing-library/react";
 import { MuiMode } from "./MuiMode";
-import { AppProviders } from "../../providers/app-providers";
+import { render, screen } from "../../test-utils";
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, { wrapper: AppProviders });
+    render(<MuiMode />);
     const headingText = screen.getByRole("heading", { name: /dark mode/i });
     expect(headingText).toHaveTextContent("dark mode");
   });
